23d42e0a59331335c052ff0283959c29f9dbfe47
AewLovely555 <thaksaporn5678@gmail.com>
2020-10-09 08:14:57 +0000

    fix site host allowed

--- myweb/models.py
+++ myweb/models.py
@@ -15,3 +15,16 @@ class Choice(models.Model):
 
     def __str__(self):
         return f'{self.question.question_text} - {self.choice_text} - {self.votes}'
+
+class MovieType(models.Model):
+    text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.text}'
+
+class Movie(models.Model):
+    movie_type = models.ForeignKey(MovieType, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.movie_type.text} - {self.name}'
